5630279afd92fe85c5e76897bbc3e00c6750d732
saurab@netkiller.com <saurab@netkiller.com>
2012-08-27 20:59:08 +0000

    Bug fixed

--- src/com/netkiller/web/sharedcontacts/upload/UploadContactsBlobstoreMapper.java
+++ src/com/netkiller/web/sharedcontacts/upload/UploadContactsBlobstoreMapper.java
@@ -41,6 +41,7 @@ import com.google.gdata.data.contacts.ContactEntry;
 import com.google.gdata.data.contacts.ContactGroupEntry;
 import com.google.gdata.data.contacts.ContactGroupFeed;
 import com.google.gdata.data.contacts.GroupMembershipInfo;
+import com.google.gdata.data.contacts.UserDefinedField;
 import com.google.gdata.data.extensions.Email;
 import com.google.gdata.data.extensions.FamilyName;
 import com.google.gdata.data.extensions.FormattedAddress;
@@ -333,7 +334,59 @@ public class UploadContactsBlobstoreMapper extends
 			contact.addPhoneNumber(mobilePhone);
 		}		
 		
-		if(!workaddress.equals("")){
+		if (!workaddress.equals("")) {
+			FormattedAddress formattedAddress = new FormattedAddress(
+					workaddress);
+			StructuredPostalAddress postalAddress = new StructuredPostalAddress();
+			postalAddress.setFormattedAddress(formattedAddress);
+			postalAddress.setRel(StaticProperties.WORK_REL);
+			// contact.addExtension(postalAddress);
+			contact.addRepeatingExtension(postalAddress);
+		}
+
+		if (!homeaddress.equals("")) {
+			FormattedAddress formattedAddress = new FormattedAddress(
+					homeaddress);
+			StructuredPostalAddress postalAddress = new StructuredPostalAddress();
+			postalAddress.setFormattedAddress(formattedAddress);
+			postalAddress.setRel(StaticProperties.HOME_REL);
+			// contact.addExtension(postalAddress);
+			contact.addRepeatingExtension(postalAddress);
+		}
+
+		if (!otheraddress.equals("")) {
+			FormattedAddress formattedAddress = new FormattedAddress(
+					otheraddress);
+			StructuredPostalAddress postalAddress = new StructuredPostalAddress();
+			postalAddress.setFormattedAddress(formattedAddress);
+			postalAddress.setRel(StaticProperties.OTHER_REL);
+			// contact.addExtension(postalAddress);
+			contact.addRepeatingExtension(postalAddress);
+		}
+
+		/*
+		 * if(!workaddress.equals("")){ PostalAddress workAddress = new
+		 * PostalAddress(); workAddress.setValue(workaddress);
+		 * workAddress.setRel(workRel); contact.addPostalAddress(workAddress); }
+		 * 
+		 * if(!homeaddress.equals("")){ PostalAddress homeAddress = new
+		 * PostalAddress(); homeAddress.setValue(homeaddress);
+		 * homeAddress.setRel(homeRel); contact.addPostalAddress(homeAddress); }
+		 * 
+		 * if(!otheraddress.equals("")){ PostalAddress otherAddress = new
+		 * PostalAddress(); otherAddress.setValue(otheraddress);
+		 * otherAddress.setRel(otherRel);
+		 * contact.addPostalAddress(otherAddress); }
+		 */
+
+		if (!notes.equals("")) {
+			// contact.setContent(new PlainTextConstruct(notes));
+			contact.getUserDefinedFields().add(
+					new UserDefinedField("Notes", notes));
+		}
+
+		
+		/*if(!workaddress.equals("")){
 			FormattedAddress formattedAddress = new FormattedAddress(workaddress);			
 			StructuredPostalAddress postalAddress =  new StructuredPostalAddress();
 			postalAddress.setFormattedAddress(formattedAddress);
@@ -359,7 +412,7 @@ public class UploadContactsBlobstoreMapper extends
 		
 		if(!notes.equals("")){
 			contact.setContent(new PlainTextConstruct(notes));
-		}		
+		}	*/	
 		
 		return contact;
 	}
